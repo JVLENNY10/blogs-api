fe2f3ee8a1553fb7792b31e98b3514cf6cafb9af
João Victor <joao.lenny100@gmail.com>
2022-04-17 22:56:14 -0300

    Mudando um detalher na migration da tabela Categories

--- sequelize/migrations/20220418001950-create-categories.js
+++ sequelize/migrations/20220418001950-create-categories.js
@@ -4,15 +4,15 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Categories', {
       id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
         allowNull: false,
-        autoIncrement: true
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
       },
 
       name: {
+        allowNull: false,
         type: Sequelize.STRING,
-        allowNull: false
       }
     });
   },
