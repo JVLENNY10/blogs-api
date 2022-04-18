1ac14ba41deb2957112e3161769af0e1d5c01494
João Victor <joao.lenny100@gmail.com>
2022-04-17 22:23:48 -0300

    Criando migration da tabela Categories

--- sequelize/migrations/20220417232646-create-users.js
+++ sequelize/migrations/20220417232646-create-users.js
@@ -1,7 +1,39 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, _Sequelize) => {
-    await queryInterface.createTable('Users');
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+
+      password: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Users');
   }
 };
