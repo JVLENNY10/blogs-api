64b1d22f01d0f7912d83c677b836565d6d004707
João Victor <joao.lenny100@gmail.com>
2022-04-22 20:10:12 -0300

    Criando controller getUserById e inserindo alguns awaits

--- controllers/controllersToGet.js
+++ controllers/controllersToGet.js
@@ -1,11 +1,14 @@
 const servicesToGet = require('../services/servicesToGet');
-// const jwtGenerator = require('../jwt/jwtGenerator');
 
-const getUsers = async (req, res) => {
-  const user = await servicesToGet.getUsers();
-  // const token = jwtGenerator({ id: user.id, email });
-
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const user = await servicesToGet.getUserById(id);
   return res.status(200).json(user);
 };
 
-module.exports = { getUsers };
+const getUsers = async (_req, res) => {
+  const user = await servicesToGet.getUsers();
+  return res.status(200).json(user);
+};
+
+module.exports = { getUserById, getUsers };
--- services/servicesToGet.js
+++ services/servicesToGet.js
@@ -1,17 +1,17 @@
 const { User } = require('../sequelize/models');
 
 const getUserByEmail = async (email) => {
-  const user = User.findOne({ where: { email } });
+  const user = await User.findOne({ where: { email } });
   return user;
 };
 
 const getUserById = async (id) => {
-  const user = User.findOne({ where: { id } });
+  const user = await User.findOne({ where: { id } });
   return user;
 };
 
 const getUsers = async () => {
-  const user = User.findAll({ attributes: { exclude: ['password'] } });
+  const user = await User.findAll({ attributes: { exclude: ['password'] } });
   return user;
 };
 
