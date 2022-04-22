2209843d22933e71737efbb28aee652cefd39f7a
João Victor <joao.lenny100@gmail.com>
2022-04-22 16:50:25 -0300

    Editando algumas middlewares para usar em /login

--- middlewares/userMiddlewares.js
+++ middlewares/userMiddlewares.js
@@ -19,6 +19,10 @@ const checkPassword = async (req, res, next) => {
     return res.status(400).json({ message: '"password" is required' });
   }
 
+  if (password === '') {
+    return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  }
+
   if (password.length !== 6) {
     return res.status(400).json({ message: '"password" length must be 6 characters long' });
   }
@@ -51,8 +55,12 @@ const emailIsValid = async (req, res, next) => {
 const emailNotNull = async (req, res, next) => {
   const { email } = req.body;
 
-  if (!email) {
+  if (email === undefined) {
     return res.status(400).json({ message: '"email" is required' });
+  }
+
+  if (email === '') {
+    return res.status(400).json({ message: '"email" is not allowed to be empty' });
   }
 
   next();
