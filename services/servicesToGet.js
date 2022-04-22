6db36a72fdc3b9170e645d64698e279c2401df0f
João Victor <joao.lenny100@gmail.com>
2022-04-22 20:07:26 -0300

    Criando service getUserById

--- services/servicesToGet.js
+++ services/servicesToGet.js
@@ -5,9 +5,14 @@ const getUserByEmail = async (email) => {
   return user;
 };
 
+const getUserById = async (id) => {
+  const user = User.findOne({ where: { id } });
+  return user;
+};
+
 const getUsers = async () => {
   const user = User.findAll({ attributes: { exclude: ['password'] } });
   return user;
 };
 
-module.exports = { getUserByEmail, getUsers };
+module.exports = { getUserByEmail, getUserById, getUsers };
