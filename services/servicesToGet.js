08597b6545336ae195f53b769aa9208d65befac7
João Victor <joao.lenny100@gmail.com>
2022-04-25 11:52:58 -0300

    Apagando service getCategoryById

--- services/servicesToGet.js
+++ services/servicesToGet.js
@@ -5,11 +5,6 @@ const getCategories = async () => {
   return categories;
 };
 
-const getCategoryById = async (id) => {
-  const category = await Category.findOne({ where: { id } });
-  return category;
-};
-
 const getUserByEmail = async (email) => {
   const user = await User.findOne({ where: { email } });
   return user;
@@ -25,4 +20,4 @@ const getUsers = async () => {
   return users;
 };
 
-module.exports = { getCategories, getCategoryById, getUserByEmail, getUserById, getUsers };
+module.exports = { getCategories, getUserByEmail, getUserById, getUsers };
