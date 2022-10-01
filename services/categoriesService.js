f57c62d533697da3e263bbf13ad59a96be7a937b
João Victor <joao.lenny100@gmail.com>
2022-10-01 19:03:40 -0300

    Services
    - Criando arquivo "categoriesService.js";
    - Criando função "create" no arquivo "categoriesService.js";
    - Criando função "getAll" no arquivo "categoriesService.js".

---
+++ services/categoriesService.js
@@ -0,0 +1,13 @@
+const { Category } = require('../sequelize/models');
+
+const create = async (name) => {
+  const newCategory = await Category.create(name);
+  return newCategory;
+};
+
+const getAll = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
+
+module.exports = { create, getAll };
