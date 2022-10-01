d58193c48166fd8030ff64436a10273dbdc2cff5
João Victor <joao.lenny100@gmail.com>
2022-10-01 19:05:20 -0300

    Controllers
    - Criando arquivo "categoriesController.js";
    - Criando função "create" no arquivo "categoriesController.js";
    - Criando função "getAll" no arquivo "categoriesController.js".

---
+++ controllers/categoriesController.js
@@ -0,0 +1,14 @@
+const categoriesService = require('../services/categoriesService');
+
+const create = async (req, res) => {
+  const name = req.body;
+  const newCategory = await categoriesService.create(name);
+  return res.status(201).json(newCategory);
+};
+
+const getAll = async (_req, res) => {
+  const categories = await categoriesService.getAll();
+  return res.status(200).json(categories);
+};
+
+module.exports = { create, getAll };
