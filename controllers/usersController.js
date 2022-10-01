8f321e620485a98e558455d9cef1b5f5355a0b82
João Victor <joao.lenny100@gmail.com>
2022-10-01 19:06:03 -0300

    Controllers
    - Criando arquivo "usersController.js";
    - Criando função "create" no arquivo "usersController.js";
    - Criando função "getAll" no arquivo "usersController.js";
    - Criando função "getById" no arquivo "usersController.js";
    - Criando função "login" no arquivo "usersController.js".

---
+++ controllers/usersController.js
@@ -0,0 +1,31 @@
+const jwtGenerator = require('../jwt/jwtGenerator');
+const usersService = require('../services/usersService');
+
+const create = async (req, res) => {
+  const { email } = req.body;
+  const { id } = await usersService.create(req.body);
+  const token = jwtGenerator({ id, email });
+
+  return res.status(201).json({ token });
+};
+
+const getAll = async (_req, res) => {
+  const users = await usersService.getAll();
+  return res.status(200).json(users);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const user = await usersService.getById(id);
+  return res.status(200).json(user);
+};
+
+const login = async (req, res) => {
+  const { email } = req.body;
+  const { id } = await usersService.login(email);
+  const token = jwtGenerator({ id, email });
+
+  return res.status(200).json({ token });
+};
+
+module.exports = { create, getAll, getById, login };
