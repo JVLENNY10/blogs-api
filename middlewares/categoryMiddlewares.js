791aa560dc0bc3e32b63b1bddec2dedae321aac9
João Victor <joao.lenny100@gmail.com>
2022-10-03 15:26:19 -0300

    Middlewares
    - Pequena alteração na função "checkCategoryName" do arquivo "categoryMiddlewares.js".

--- middlewares/categoryMiddlewares.js
+++ middlewares/categoryMiddlewares.js
@@ -1,7 +1,7 @@
 const checkCategoryName = (req, res, next) => {
   const { name } = req.body;
 
-  if (!name) {
+  if (name === undefined) {
     return res.status(400).json({ message: '"name" is required' });
   }
 
