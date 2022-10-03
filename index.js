e6d38aba31d977bce641751c7f7f46322c9b3c94
João Victor <joao.lenny100@gmail.com>
2022-10-03 15:37:05 -0300

    index.js
    - Removendo as rotas;
    - Importando os arquivos da pasta routes e utilizando.

--- index.js
+++ index.js
@@ -4,50 +4,15 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const {
-  getBlogPosts, getCategories, getUserById, getUsers,
-} = require('./controllers/controllersToGet');
-
-const {
-  createBlogPost, createCategory, createUser, loginUser,
-} = require('./controllers/controllersToPost');
-
-const checkBlogPostInformations = require('./middlewares/blogPostMiddlewares');
-const checkCategoryName = require('./middlewares/categoryMiddlewares');
-const {
-  authToken,
-  checkDisplayName,
-  checkEmailExists,
-  checkEmailIsNotNull,
-  checkEmailIsValid,
-  checkEmailNotExists,
-  checkPassword,
-  checkUserExistsById,
-} = require('./middlewares/userMiddlewares');
+const usersRoutes = require('./routes/usersRoutes');
+const blogPostsRoutes = require('./routes/blogPostsRoutes');
+const categoriesRoutes = require('./routes/categoriesRoutes');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_req, res) => res.send());
 
-app.get('/categories', authToken, getCategories);
-app.post('/categories', authToken, checkCategoryName, createCategory);
-
-app.post(
-  '/login', checkEmailIsNotNull, checkEmailIsValid, checkEmailNotExists, checkPassword, loginUser,
-);
-
-app.get('/post', authToken, getBlogPosts);
-app.post('/post', authToken, checkBlogPostInformations, createBlogPost);
-
-app.get('/user', authToken, getUsers);
-app.get('/user/:id', authToken, checkUserExistsById, getUserById);
-app.post(
-  '/user',
-  checkDisplayName,
-  checkEmailIsNotNull,
-  checkEmailIsValid,
-  checkEmailExists,
-  checkPassword,
-  createUser,
-);
+app.use(blogPostsRoutes);
+app.use(categoriesRoutes);
+app.use(usersRoutes);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
