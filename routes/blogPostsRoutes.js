ead39945b2327bf7941e38fce89633cda8088d31
João Victor <joao.lenny100@gmail.com>
2022-10-03 21:48:32 -0300

    Routes
    - Criando rota "/post/:id" do tipo PUT no arquivo "blogPostsRoutes.js".

--- routes/blogPostsRoutes.js
+++ routes/blogPostsRoutes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const { checkToken } = require('../middlewares/userMiddlewares');
-const { create, getAll, getById } = require('../controllers/blogPostsController');
+const { create, getAll, getById, update } = require('../controllers/blogPostsController');
 const {
   checkById,
   checkCategoryIds,
+  checkCategoryIdsInUpdate,
   checkContent,
   checkTitle,
+  checkUserId,
 } = require('../middlewares/blogPostMiddlewares');
 
 const routes = Router();
@@ -13,5 +15,14 @@ const routes = Router();
 routes.get('/post', checkToken, getAll);
 routes.get('/post/:id', checkToken, checkById, getById);
 routes.post('/post', checkToken, checkCategoryIds, checkContent, checkTitle, create);
+routes.put(
+  '/post/:id',
+  checkToken,
+  checkUserId,
+  checkCategoryIdsInUpdate,
+  checkContent,
+  checkTitle,
+  update,
+);
 
 module.exports = routes;
