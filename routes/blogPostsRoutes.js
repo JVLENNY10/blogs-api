b1b42bc330ec5eedff1c7f1411caae600582a6e2
João Victor <joao.lenny100@gmail.com>
2022-10-04 11:28:43 -0300

    Routes
    - Criando rota "/post/search" do tipo GET no arquivo "blogPostsRoutes.js".

--- routes/blogPostsRoutes.js
+++ routes/blogPostsRoutes.js
@@ -1,6 +1,13 @@
 const { Router } = require('express');
 const { checkToken } = require('../middlewares/userMiddlewares');
-const { create, destroy, getAll, getById, update } = require('../controllers/blogPostsController');
+const {
+  create,
+  destroy,
+  getAll,
+  getById,
+  getBySearchTerm,
+  update,
+} = require('../controllers/blogPostsController');
 const {
   checkById,
   checkCategoryIds,
@@ -14,6 +21,7 @@ const routes = Router();
 
 routes.delete('/post/:id', checkToken, checkById, checkUserId, destroy);
 routes.get('/post', checkToken, getAll);
+routes.get('/post/search', checkToken, getBySearchTerm);
 routes.get('/post/:id', checkToken, checkById, getById);
 routes.post('/post', checkToken, checkCategoryIds, checkContent, checkTitle, create);
 routes.put(
@@ -25,5 +33,5 @@ routes.put(
   checkTitle,
   update,
 );
-
+  
 module.exports = routes;
