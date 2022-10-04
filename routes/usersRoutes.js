b5eb74ae4ad4ace7ca998b67e9b49da3f24a2e30
João Victor <joao.lenny100@gmail.com>
2022-10-03 22:19:36 -0300

    Routes
    - Criando rota "/user/me" do tipo DELETE no arquivo "usersRoutes.js".

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { create, getAll, getById, login } = require('../controllers/usersController');
+const { create, destroy, getAll, getById, login } = require('../controllers/usersController');
 const {
   checkToken,
   checkById,
@@ -12,6 +12,7 @@ const {
 
 const routes = Router();
 
+routes.delete('/user/me', checkToken, destroy);
 routes.get('/user', checkToken, getAll);
 routes.get('/user/:id', checkToken, checkById, getById);
 routes.post('/user', checkDisplayName, checkEmail, checkCreate, checkPassword, create);
